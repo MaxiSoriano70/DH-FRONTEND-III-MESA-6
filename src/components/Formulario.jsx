6e60506f13d10ebfbd3895fc9c69c58aa67eaba0
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2024-08-13 20:42:12 -0300

    Agregando evento onBlur

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -12,6 +12,10 @@ const Formulario = () => {
     const [edadValido, setEdadValido] = useState(false);
     const [pokemonValido, setPokemonValido] = useState(false);
 
+    const [nombreError, setNombreError] = useState(true);
+    const [edadError, setEdadError] = useState(true);
+    const [pokemonError, setPokemonError] = useState(true);
+
     const resetFormulario = () => {
         setUsuarioDatos({
             nombre: "",
@@ -21,22 +25,28 @@ const Formulario = () => {
         setNombreValido(false);
         setEdadValido(false);
         setPokemonValido(false);
+        setNombreError(true);
+        setEdadError(true);
+        setPokemonError(true);
         setShow(false);
     };
 
     const handleChangeNombre = (e) => {
         setUsuarioDatos({ ...usuarioDatos, nombre: e.target.value });
         setNombreValido(validarNombre(e.target.value));
+        setNombreError(validarNombre(e.target.value));
     };
 
     const handleChangeEdad = (e) => {
         setUsuarioDatos({ ...usuarioDatos, edad: e.target.value });
         setEdadValido(validarEdad(e.target.value));
+        setEdadError(validarEdad(e.target.value));
     };
 
     const handleChangePokemon = (e) => {
         setUsuarioDatos({ ...usuarioDatos, pokemon: e.target.value });
         setPokemonValido(validarPokemon(e.target.value));
+        setPokemonError(validarPokemon(e.target.value));
     };
 
     const validarNombre = (nombre) => {
@@ -74,6 +84,18 @@ const Formulario = () => {
         }
         else{
             setShow(false);
+            if(!nombreValido){
+                setNombreError(false);
+                setShow(false);
+            }
+            if(!edadValido){
+                setEdadError(false);
+                setShow(false);
+            }
+            if(!pokemonValido){
+                setPokemonError(false);
+                setShow(false);
+            }
         }
     };
 
@@ -98,15 +120,16 @@ const Formulario = () => {
                     <label className="label" htmlFor="inputNombreApelldo">Nombre y apellido</label>
                     <div className="control">
                         <input
-                            className={`input ${nombreValido ? "is-success" : "is-danger"}`}
+                            className={`input ${nombreError ? "is-success" : "is-danger"}`}
                             value={usuarioDatos.nombre}
                             type="text"
                             id="inputNombreApelldo"
                             onChange={handleChangeNombre}
+                            onBlur={handleChangeNombre}
                             placeholder="Ingrese nombre y apellido..."
                         />
                     </div>
-                    {!nombreValido ?
+                    {!nombreError ?
                     <article className="m-2 message is-danger">
                         <div className="message-body">
                             Error ingrese su nombre y apellido.
@@ -119,15 +142,16 @@ const Formulario = () => {
                     <label className="label" htmlFor="inputEdad">Edad</label>
                     <div className="control">
                         <input
-                            className={`input ${edadValido ? "is-success" : "is-danger"}`}
+                            className={`input ${edadError ? "is-success" : "is-danger"}`}
                             value={usuarioDatos.edad}
                             type="number"
                             id="inputEdad"
                             onChange={handleChangeEdad}
+                            onBlur={handleChangeEdad}
                             placeholder="Ingrese edad..."
                         />
                     </div>
-                    {!edadValido ?
+                    {!edadError ?
                     <article className="m-2 message is-danger">
                         <div className="message-body">
                             Error ingrese su edad.
@@ -139,15 +163,15 @@ const Formulario = () => {
                 <div className="field m-4">
                     <label className="label" htmlFor="pokemon">Pokemon</label>
                     <div className="control">
-                        <div className={`select ${pokemonValido ? "is-success" : "is-danger"}`}>
-                            <select id="pokemon" value={usuarioDatos.pokemon} onChange={handleChangePokemon}>
+                        <div className={`select ${pokemonError ? "is-success" : "is-danger"}`}>
+                            <select id="pokemon" value={usuarioDatos.pokemon} onChange={handleChangePokemon} onBlur={handleChangePokemon}>
                                 <option value="">Seleccione un Pokemon</option>
                                 <option value="Lucario">Lucario</option>
                                 <option value="Charizard">Charizard</option>
                                 <option value="Buizel">Buizel</option>
                             </select>
                         </div>
-                        {!pokemonValido?
+                        {!pokemonError?
                         <article className="m-2 message is-danger">
                             <div className="message-body">
                                 Error eliga un pokemon.
