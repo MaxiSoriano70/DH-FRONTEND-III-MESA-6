b9210fd79f04bcdf95b541f7cd87875c5233aad9
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2024-08-13 04:23:21 -0300

    Mesa 6

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,174 @@
+import { useState } from "react";
+
+const Formulario = () => {
+    const [usuarioDatos, setUsuarioDatos] = useState({
+        nombre: "",
+        edad: "",
+        pokemon: ""
+    });
+
+    const [show, setShow] = useState(false);
+    const [nombreValido, setNombreValido] = useState(false);
+    const [edadValido, setEdadValido] = useState(false);
+    const [pokemonValido, setPokemonValido] = useState(false);
+
+    const resetFormulario = () => {
+        setUsuarioDatos({
+            nombre: "",
+            edad: "",
+            pokemon: ""
+        });
+        setNombreValido(false);
+        setEdadValido(false);
+        setPokemonValido(false);
+        setShow(false);
+    };
+
+    const handleChangeNombre = (e) => {
+        setUsuarioDatos({ ...usuarioDatos, nombre: e.target.value });
+        setNombreValido(validarNombre(e.target.value));
+    };
+
+    const handleChangeEdad = (e) => {
+        setUsuarioDatos({ ...usuarioDatos, edad: e.target.value });
+        setEdadValido(validarEdad(e.target.value));
+    };
+
+    const handleChangePokemon = (e) => {
+        setUsuarioDatos({ ...usuarioDatos, pokemon: e.target.value });
+        setPokemonValido(validarPokemon(e.target.value));
+    };
+
+    const validarNombre = (nombre) => {
+        if(nombre.trim().length >= 3 && nombre.includes(" ")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    };
+
+    const validarEdad = (edad) => {
+        const auxEdad = parseInt(edad, 10) || "";
+        if(auxEdad >= 1 && auxEdad !== ""){
+            return true;
+        }
+        else{
+            return false;
+        }
+    };
+
+    const validarPokemon = (pokemon) => {
+        if(pokemon === "Lucario" || pokemon === "Charizard" || pokemon === "Buizel"){
+            return true;
+        }
+        else{
+            return false;
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(nombreValido && edadValido && pokemonValido){
+            setShow(true);
+        }
+        else{
+            setShow(false);
+        }
+    };
+
+    return (
+        <div className="container is-max-tablet">
+            {show?
+                <div className="card has-background-primary">
+                    <div className="card-content">
+                        <div className="content">
+                            <p className="has-text-primary-invert">Nombre: {usuarioDatos.nombre}</p>
+                            <p className="has-text-primary-invert">Edad: {usuarioDatos.edad}</p>
+                            <p className="has-text-primary-invert">Pokemon: {usuarioDatos.pokemon}</p>
+                        </div>
+                        <div className="control">
+                            <button className="button is-link is-dark" onClick={resetFormulario}>Volver a cargar datos</button>
+                        </div>
+                    </div>
+                </div>
+            :
+            <form onSubmit={handleSubmit}>
+                <div className="field m-4">
+                    <label className="label" htmlFor="inputNombreApelldo">Nombre y apellido</label>
+                    <div className="control">
+                        <input
+                            className={`input ${nombreValido ? "is-success" : "is-danger"}`}
+                            value={usuarioDatos.nombre}
+                            type="text"
+                            id="inputNombreApelldo"
+                            onChange={handleChangeNombre}
+                            placeholder="Ingrese nombre y apellido..."
+                        />
+                    </div>
+                    {!nombreValido ?
+                    <article className="m-2 message is-danger">
+                        <div className="message-body">
+                            Error ingrese su nombre y apellido.
+                        </div>
+                    </article>:
+                    ""
+                    }
+                </div>
+                <div className="field m-4">
+                    <label className="label" htmlFor="inputEdad">Edad</label>
+                    <div className="control">
+                        <input
+                            className={`input ${edadValido ? "is-success" : "is-danger"}`}
+                            value={usuarioDatos.edad}
+                            type="number"
+                            id="inputEdad"
+                            onChange={handleChangeEdad}
+                            placeholder="Ingrese edad..."
+                        />
+                    </div>
+                    {!edadValido ?
+                    <article className="m-2 message is-danger">
+                        <div className="message-body">
+                            Error ingrese su edad.
+                        </div>
+                    </article>:
+                    ""
+                    }
+                </div>
+                <div className="field m-4">
+                    <label className="label" htmlFor="pokemon">Pokemon</label>
+                    <div className="control">
+                        <div className={`select ${pokemonValido ? "is-success" : "is-danger"}`}>
+                            <select id="pokemon" value={usuarioDatos.pokemon} onChange={handleChangePokemon}>
+                                <option value="">Seleccione un Pokemon</option>
+                                <option value="Lucario">Lucario</option>
+                                <option value="Charizard">Charizard</option>
+                                <option value="Buizel">Buizel</option>
+                            </select>
+                        </div>
+                        {!pokemonValido?
+                        <article className="m-2 message is-danger">
+                            <div className="message-body">
+                                Error eliga un pokemon.
+                            </div>
+                        </article>:
+                        ""
+                        }
+                    </div>
+                </div>
+                <div className="field m-4 is-grouped">
+                    <div className="control">
+                        <button className="button is-link" type="submit">Enviar</button>
+                    </div>
+                    <div className="control">
+                        <button className="button is-link is-light" type="reset" onClick={resetFormulario}>Cancelar</button>
+                    </div>
+                </div>
+            </form>
+            }
+        </div>
+    );
+};
+
+export default Formulario;
